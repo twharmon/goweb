797790ac3e3cb618b3a0d6472e177aa86eea8d48
Travis Harmon <harmtrav@gmail.com>
2019-11-18 16:26:30 -0500

    add exampls

---
+++ examples/logging/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/twharmon/goweb"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln(err)
+	}
+
+	app := goweb.New()
+
+	// uses std lib "log"
+	app.AddStdLogger(goweb.LogLevelDebug)
+
+	// uses SLACK_LOG_WEBHOOK environment variable
+	app.AddSlackLogger(goweb.LogLevelError)
+
+	// create your own logger
+	app.AddCustomLogger(&myLogger{goweb.LogLevelNotice})
+
+	app.GET("/divide/{a}/by/{b}", divide)
+
+	app.Run(":8080")
+}
+
+func divide(c *goweb.Context) goweb.Responder {
+	a, err := strconv.ParseFloat(c.Param("a"), 64)
+	if err != nil {
+		c.LogNotice(fmt.Sprintf("attempted to divide %s", c.Param("a")))
+		return c.BadRequest().PlainText("only numbers can be divided")
+	}
+	b, err := strconv.ParseFloat(c.Param("b"), 64)
+	if err != nil {
+		c.LogNotice(fmt.Sprintf("attempted to divide %s", c.Param("b")))
+		return c.BadRequest().PlainText("only numbers can be divided")
+	}
+
+	if b == 0 {
+		c.LogEmergency("division by zero")
+		return c.BadRequest().PlainText("can not divide by zero")
+	}
+
+	return c.OK().PlainText(fmt.Sprintf("%f", a/b))
+}
+
+type myLogger struct {
+	minLevel int
+}
+
+func (l *myLogger) Log(logLevel int, message interface{}) {
+	if logLevel >= l.minLevel {
+		log.Printf("Notice - %v", message)
+	}
+}
---
+++ examples/middleware/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/twharmon/goweb"
+)
+
+// User contains user information.
+type User struct {
+	ID   uint64 `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	app := goweb.New()
+
+	auth := goweb.NewMiddleware()
+	auth.Use(isAuthUser, isNotGopher)
+
+	app.GET("/", hello)
+	app.GET("/user/{name}", auth.Apply(getUser))
+
+	app.Run(":8080")
+}
+
+func isAuthUser(c *goweb.Context) goweb.Responder {
+	c.Set("user", &User{
+		ID:   45,
+		Name: c.Param("name"),
+	})
+
+	// return nil to continue the middleware chain
+	return nil
+}
+
+func isNotGopher(c *goweb.Context) goweb.Responder {
+	user := c.Get("user").(*User)
+	if strings.ToLower(user.Name) == "gopher" {
+		// return non nil Responder to terminate middleware chain
+		return c.Forbidden().PlainText("no gophers allowed")
+	}
+
+	// return nil to continue the middleware chain
+	return nil
+}
+
+func hello(c *goweb.Context) goweb.Responder {
+	return c.OK().JSON(goweb.Map{
+		"hello": "world",
+	})
+}
+
+func getUser(c *goweb.Context) goweb.Responder {
+	return c.OK().JSON(c.Get("user").(*User))
+}
