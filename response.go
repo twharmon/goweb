92e64a6ef890d2828e83a527816b7e9f994b201e
Travis Harmon <harmtrav@gmail.com>
2020-01-22 18:15:05 -0500

    compare gzipped modtime to original modtime

--- response.go
+++ response.go
@@ -3,6 +3,7 @@ package goweb
 import (
 	"compress/gzip"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -55,61 +56,64 @@ func (r *JSONResponse) Respond() {
 
 // Respond sends a JSON response.
 func (r *FileResponse) Respond() {
-	if r.gzip > 0 {
-		if !strings.Contains(r.context.Request.Header.Get(acceptEncodingHeader), contentEncodingGzip) {
-			http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-			return
-		}
-
-		fi, err := os.Stat(r.path)
-		if err != nil {
-			r.context.LogError(err)
-			http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-			return
-		}
-		if fi.Size() < r.gzip {
-			http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-			return
-		}
-
-		gzipPath := r.path + ".gz"
-		if _, err := os.Stat(gzipPath); errors.Is(err, os.ErrNotExist) {
-			f, err := os.Open(r.path)
-			if err != nil {
-				r.context.LogError(err)
-				http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-				return
-			}
-			defer f.Close()
-
-			fgz, err := os.Create(gzipPath)
-			if err != nil {
-				r.context.LogError(err)
-				http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-				return
-			}
-			defer fgz.Close()
-
-			gw := gzip.NewWriter(fgz)
-			_, err = io.Copy(gw, f)
-			if err != nil {
-				r.context.LogError(err)
-				http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-				return
-			}
-			err = gw.Close()
-			if err != nil {
-				r.context.LogError(err)
-				http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
-				return
-			}
-		}
-		r.context.ResponseWriter.Header().Set(contentEncodingHeader, contentEncodingGzip)
-		r.context.ResponseWriter.Header().Set(contentTypeHeader, contentTypeWasm)
-		http.ServeFile(r.context.ResponseWriter, r.context.Request, gzipPath)
-	} else {
+	if r.gzip == 0 {
 		http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
+		return
 	}
+
+	if !strings.Contains(r.context.Request.Header.Get(acceptEncodingHeader), contentEncodingGzip) {
+		http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
+		return
+	}
+
+	fi, err := os.Stat(r.path)
+	if err != nil {
+		r.context.LogError(fmt.Errorf("unable to stat file: %w", err))
+		http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
+		return
+	}
+	if fi.Size() < r.gzip {
+		http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
+		return
+	}
+
+	gzipPath := r.path + ".gz"
+	if err := r.gzipIfNeeded(r.path, gzipPath, fi); err != nil {
+		r.context.LogError(fmt.Errorf("gzip error: %w", err))
+		http.ServeFile(r.context.ResponseWriter, r.context.Request, r.path)
+		return
+	}
+
+	r.context.ResponseWriter.Header().Set(contentEncodingHeader, contentEncodingGzip)
+	r.context.ResponseWriter.Header().Set(contentTypeHeader, contentTypeWasm)
+	http.ServeFile(r.context.ResponseWriter, r.context.Request, gzipPath)
+}
+
+func (r *FileResponse) gzipIfNeeded(path string, gzipPath string, fi os.FileInfo) error {
+	if gfi, err := os.Stat(gzipPath); errors.Is(err, os.ErrNotExist) || gfi.ModTime().Before(fi.ModTime()) {
+		f, err := os.Open(r.path)
+		if err != nil {
+			return fmt.Errorf("unable to open file: %w", err)
+		}
+		defer f.Close()
+
+		fgz, err := os.Create(gzipPath)
+		if err != nil {
+			return fmt.Errorf("unable to create gzip file: %w", err)
+		}
+		defer fgz.Close()
+
+		gw := gzip.NewWriter(fgz)
+		_, err = io.Copy(gw, f)
+		if err != nil {
+			return fmt.Errorf("unable to write to file: %w", err)
+		}
+		err = gw.Close()
+		if err != nil {
+			return fmt.Errorf("unable to close gzipper: %w", err)
+		}
+	}
+	return nil
 }
 
 // Respond sends a plain text response.
