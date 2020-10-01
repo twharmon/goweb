7f4385dcdbd0e2ca3b25bcd32100ebe5c4c0679a
Travis Harmon <twharmon@gmail.com>
2020-10-01 12:46:41 -0500

    Fix tests

--- logger_test.go
+++ logger_test.go
@@ -7,33 +7,33 @@ import (
 )
 
 func TestLogLevelDebug(t *testing.T) {
-	equals(t, goweb.LogLevelDebug.String(), "Debug")
+	equals(t, goweb.LogLevelDebug.String(), "DEBUG")
 }
 
 func TestLogLevelInfo(t *testing.T) {
-	equals(t, goweb.LogLevelInfo.String(), "Info")
+	equals(t, goweb.LogLevelInfo.String(), "INFO")
 }
 
 func TestLogLevelNotice(t *testing.T) {
-	equals(t, goweb.LogLevelNotice.String(), "Notice")
+	equals(t, goweb.LogLevelNotice.String(), "NOTICE")
 }
 
 func TestLogLevelWarning(t *testing.T) {
-	equals(t, goweb.LogLevelWarning.String(), "Warning")
+	equals(t, goweb.LogLevelWarning.String(), "WARNING")
 }
 
 func TestLogLevelError(t *testing.T) {
-	equals(t, goweb.LogLevelError.String(), "Error")
+	equals(t, goweb.LogLevelError.String(), "ERROR")
 }
 
 func TestLogLevelCritical(t *testing.T) {
-	equals(t, goweb.LogLevelCritical.String(), "Critical")
+	equals(t, goweb.LogLevelCritical.String(), "CRITICAL")
 }
 
 func TestLogLevelAlert(t *testing.T) {
-	equals(t, goweb.LogLevelAlert.String(), "Alert")
+	equals(t, goweb.LogLevelAlert.String(), "ALERT")
 }
 
 func TestLogLevelEmergency(t *testing.T) {
-	equals(t, goweb.LogLevelEmergency.String(), "Emergency")
+	equals(t, goweb.LogLevelEmergency.String(), "EMERGENCY")
 }
